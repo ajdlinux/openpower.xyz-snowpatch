3f6ec95c78478fcb2f1c42cf31f6be675fab8e73
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2018-02-05 13:29:55 +1100

    patchwork-test: Initial script
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

---
+++ patchwork-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This build script is for running the Jenkins builds using docker.
+#
+
+# Trace bash processing
+#set -x
+
+# Default variables
+WORKSPACE="${WORKSPACE:-${HOME}/${RANDOM}${RANDOM}}"
+
+# Timestamp for job
+echo "Build started, $(date)"
+
+mkdir -p "${WORKSPACE}"
+
+sed -i "s/UID=1000/UID=$UID/g" patchwork/tools/docker/Dockerfile
+docker-compose build
+docker-compose run --rm web --tox
+docker-compose rm -fs
+
+# Timestamp for build
+echo "Build completed, $(date)"
+
