33844a9ebd67e25acf79918f9cc1936e9b212f74
Russell Currey <ruscur@russell.cc>
2018-07-18 16:33:53 +1000

    Add dtc to the skiboot docker container
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- skiboot-build.sh
+++ skiboot-build.sh
@@ -30,7 +30,8 @@ RUN apt-get update && apt-get install -yy \
 	git \
 	software-properties-common \
 	libssl-dev \
-	valgrind
+	valgrind \
+	device-tree-compiler
 
 RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
 RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
